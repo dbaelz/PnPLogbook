5ab2497acd4cd5289682b4489fafe01ea90b6279
Daniel Bälz <info@dbaelz.de>
2024-10-19 23:34:54 +0200

    Client: Improve overview screen

--- composeApp/src/commonMain/kotlin/de/dbaelz/pnp/logbook/features/overview/OverviewScreen.kt
+++ composeApp/src/commonMain/kotlin/de/dbaelz/pnp/logbook/features/overview/OverviewScreen.kt
@@ -1,23 +1,54 @@
 package de.dbaelz.pnp.logbook.features.overview
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import de.dbaelz.pnp.logbook.navigation.Screen
 
 @Composable
 fun OverviewScreen(navigateTo: (Screen) -> Unit) {
-    // TODO: Add proper UI
-    Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
-        Text("Overview")
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OverviewCard(Screen.Experience) { navigateTo(it) }
 
-        Button(
-            onClick = { navigateTo(Screen.Experience) }
+        OverviewCard(Screen.Currency) { navigateTo(it) }
+
+        OverviewCard(Screen.Logbook) { navigateTo(it) }
+    }
+}
+
+@Composable
+private fun OverviewCard(screen: Screen, navigateTo: (Screen) -> Unit) {
+    Card(
+        modifier = Modifier
+            .height(96.dp)
+            .widthIn(0.dp, 512.dp)
+            .clickable { navigateTo(screen) },
+        elevation = 4.dp
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
         ) {
-            Text("Experience")
+            Text(
+                text = screen.title,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
         }
     }
 }
